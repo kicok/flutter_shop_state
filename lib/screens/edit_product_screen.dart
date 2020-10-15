a73005f42ada12743182b7719947c7d0ba51d68b
kicok <kicoker@gmail.com>
2020-10-15 11:10:14 +0900

    Updating Data via PATCH Requests

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -110,12 +110,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
     print(_editedProduct.imageUrl);
 
     if (_editedProduct.id != null) {
-      Provider.of<Products>(context, listen: false)
+      await Provider.of<Products>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
-      setState(() {
-        _isLoading = false;
-      });
-      Navigator.of(context).pop();
     } else {
       try {
         await Provider.of<Products>(context, listen: false)
@@ -138,13 +134,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ],
           ),
         );
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-        Navigator.of(context).pop();
       }
+      // finally {
+      //   setState(() {
+      //     _isLoading = false;
+      //   });
+      //   Navigator.of(context).pop();
+      // }
     }
+    setState(() {
+      _isLoading = false;
+    });
+    Navigator.of(context).pop();
   }
 
   @override
