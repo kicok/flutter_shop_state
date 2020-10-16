a8906b4393ecd9b8f554273e4c64ed18022e7c95
kicok <kicoker@gmail.com>
2020-10-16 17:46:54 +0900

    Allowing Users to Log in

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -9,9 +9,10 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC9OkGTbf1aDe2j6THNB2fEj2gpDBfxJ10';
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyC9OkGTbf1aDe2j6THNB2fEj2gpDBfxJ10';
 
     final response = await http.post(
       url,
@@ -24,5 +25,13 @@ class Auth with ChangeNotifier {
       ),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> signin(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -115,6 +115,10 @@ class _AuthCardState extends State<AuthCard> {
     });
     if (_authMode == AuthMode.Login) {
       // Log user in
+      await Provider.of<Auth>(context, listen: false).signin(
+        _authData['email'],
+        _authData['password'],
+      );
     } else {
       // Sign user up
 
