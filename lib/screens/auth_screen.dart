971b906f45267ed14099c0798ad437f55e92af91
kicok <kicoker@gmail.com>
2020-10-21 17:58:02 +0900

    Working with the "AnimatedContainer"

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -105,27 +105,27 @@ class _AuthCardState extends State<AuthCard>
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
-  AnimationController _controller;
-  Animation<Size> _heightAnimation;
+  // AnimationController _controller;
+  // Animation<Size> _heightAnimation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(
-        milliseconds: 300,
-      ),
-    );
-    _heightAnimation = Tween<Size>(
-      begin: Size(double.infinity, 260),
-      end: Size(double.infinity, 320),
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.linear,
-      ),
-    );
+    // _controller = AnimationController(
+    //   vsync: this,
+    //   duration: Duration(
+    //     milliseconds: 300,
+    //   ),
+    // );
+    // _heightAnimation = Tween<Size>(
+    //   begin: Size(double.infinity, 260),
+    //   end: Size(double.infinity, 320),
+    // ).animate(
+    //   CurvedAnimation(
+    //     parent: _controller,
+    //     curve: Curves.linear,
+    //   ),
+    // );
 
     // _heightAnimation.addListener(() => setState(() {}));
   }
@@ -133,7 +133,7 @@ class _AuthCardState extends State<AuthCard>
   @override
   void dispose() {
     super.dispose();
-    _controller.dispose();
+    //_controller.dispose();
   }
 
   void _showErrorDialog(String message) {
@@ -208,12 +208,12 @@ class _AuthCardState extends State<AuthCard>
       setState(() {
         _authMode = AuthMode.Signup;
       });
-      _controller.forward(); // 가입모드시 높이가 높아짐
+      //  _controller.forward(); // 가입모드시 높이가 높아짐
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
-      _controller.reverse(); //로그인 모드시 높이가 작아짐
+      //  _controller.reverse(); //로그인 모드시 높이가 작아짐
     }
   }
 
@@ -225,17 +225,15 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-            // ch는 상단의 child: AnimatedBuilder(...) 부분을 가리킴
-            //height: _authMode == AuthMode.Signup ? 320 : 260,
-            height: _heightAnimation.value.height,
-            constraints:
-                BoxConstraints(minHeight: _heightAnimation.value.height),
-            width: deviceSize.width * 0.75,
-            padding: EdgeInsets.all(16.0),
-            child: ch),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        //height: _heightAnimation.value.height,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
