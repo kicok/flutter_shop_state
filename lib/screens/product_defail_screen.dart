a34957db147c5470b5f1811c5f5b3234983f1a5b
kicok <kicoker@gmail.com>
2020-10-22 17:28:42 +0900

    Working with "Slivers"

--- lib/screens/product_defail_screen.dart
+++ lib/screens/product_defail_screen.dart
@@ -19,26 +19,37 @@ class ProductDetailScreen extends StatelessWidget {
       listen: false,
     ).findByid(productId);
     return Scaffold(
-        appBar: AppBar(
-          title: Text(loadedProduct.title),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 300,
-                width: double.infinity,
-                child: Hero(
-                    tag: loadedProduct.id,
-                    child: Image.network(loadedProduct.imageUrl,
-                        fit: BoxFit.cover)),
+        // appBar: AppBar(
+        //   title: Text(loadedProduct.title),
+        // ),
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: 300,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(loadedProduct.title),
+            background: Hero(
+              tag: loadedProduct.id,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
               ),
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
               SizedBox(height: 10),
-              Text('\$${loadedProduct.price}',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 20,
-                  )),
+              Text(
+                '\$${loadedProduct.price}',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
               SizedBox(height: 10),
               Container(
                 padding: EdgeInsets.symmetric(horizontal: 10),
@@ -48,9 +59,12 @@ class ProductDetailScreen extends StatelessWidget {
                   textAlign: TextAlign.center,
                   softWrap: true,
                 ),
-              )
+              ),
+              SizedBox(height: 800)
             ],
           ),
-        ));
+        )
+      ],
+    ));
   }
 }
